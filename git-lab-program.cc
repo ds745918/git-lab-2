ff47ebdb2f60cbccdfd2dce63586cab0903bca54
Devin Swiatek <89610689+ds745918@users.noreply.github.com>
2021-09-02 18:12:56 -0400

    Add files via upload

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,1 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Devin SWiatek
+ *        Date: 9/2/21
+ * Description: hello to github
+ */
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
